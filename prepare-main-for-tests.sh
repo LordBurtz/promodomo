5d9ee13abffca620de5110eb684b80b893bf30af
fridolin.karger <fridolin.karger@gmail.com>
2025-05-15 20:10:52 +0200

    update script II

--- prepare-main-for-tests.sh
+++ prepare-main-for-tests.sh
@@ -2,6 +2,6 @@
 
 if [ "$(uname)" == "Darwin" ]; then
     sed -i '' 's/import Week/import SolutionWeek/g' test/*tests.hs
-else ; then
+else
     sed -i 's/import Week/import SolutionWeek/g' test/*tests.hs
 fi
